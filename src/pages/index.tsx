a9f77db1dfefccd48bcce14c2d1410f457b4f42d
Guilherme Barbosa <me@guibarbosa.dev>
2024-02-26 20:23:57 -0300

    feat: add "main projects" section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Metadata } from "next";
+import { GetStaticProps, Metadata } from "next";
+import { promises as fs } from "fs";
 
 type SocialLink = {
   href: string;
@@ -30,7 +31,26 @@ export const metada: Metadata = {
   description: `Hello, I am Guilherme Barbosa, a software developer, this is my personal website.`,
 };
 
-export default function Home() {
+export interface MainProject {
+  title: string;
+  problem: string;
+  solution: string;
+  participation: string;
+  technologies: string;
+  screenshots: string[];
+}
+
+export const getStaticProps = async () => {
+  const file = await fs.readFile(
+    process.cwd() + "/public/main-projects.json",
+    "utf8"
+  );
+  const projects = JSON.parse(file);
+
+  return { props: { projects } };
+};
+
+export default function Home(props: { projects: MainProject[] }) {
   return (
     <>
       <Head>
@@ -39,7 +59,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon/favicon.ico" />
       </Head>
-      <div>
+      <>
         <main
           className={`${styles.main} ${styles.section}  ${inter.className}`}
         >
@@ -61,7 +81,39 @@ export default function Home() {
           <p>I love using code to solve real life problems.</p>
           <p>Developing applications professionally since 2018.</p>
         </main>
-      </div>
+        <section className={styles.section} title="Main projects">
+          <h2>Main projects</h2>
+          {props.projects.map((project, index) => (
+            <div className={styles.card} key={index}>
+              <div className={styles.cardContent}>
+                <h3>{project.title}</h3>
+                <br></br>
+                <div>
+                  <div>Main problem: </div>
+                  <p>{project.problem}</p>
+                </div>
+
+                <br></br>
+                <div>
+                  <div>Solution: </div>
+                  <p>{project.solution}</p>
+                </div>
+              </div>
+              <div className={styles.cardFooter}>
+                {project.screenshots.map((imageUrl, index) => (
+                  <Image
+                    key={imageUrl}
+                    src={imageUrl}
+                    width={150}
+                    height={100}
+                    alt={`${project.title}'s ${index + 1} image`}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+        </section>
+      </>
     </>
   );
 }
