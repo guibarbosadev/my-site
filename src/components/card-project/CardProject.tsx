302ca5833e86b9fe2a7044be0271702dab352958
Guilherme Barbosa <me@guibarbosa.dev>
2024-03-11 18:48:57 -0300

    feat: collapse 'project card' text & show 'see more' button

--- src/components/card-project/CardProject.tsx
+++ src/components/card-project/CardProject.tsx
@@ -1,15 +1,24 @@
 import { MainProject } from "@/types";
 import Image from "next/image";
 import classNames from "./CardProject.module.css";
+import { useState } from "react";
 
 type Props = {
   project: MainProject;
 };
 
 export default function CardProject({ project }: Props) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
   return (
     <div className={classNames.card}>
-      <div className={classNames.cardContent}>
+      <div
+        className={`${classNames.cardContent} ${
+          isCollapsed
+            ? classNames.cardContentCollapsed
+            : classNames.cardContentExpanded
+        }`}
+      >
         <h3>{project.title}</h3>
         <br></br>
         <div>
@@ -34,6 +43,13 @@ export default function CardProject({ project }: Props) {
           <div>Impact: </div>
           <p>{project.impact}</p>
         </div>
+
+        <button
+          className={classNames.btnCollapse}
+          onClick={() => setIsCollapsed((flag) => !flag)}
+        >
+          {isCollapsed ? "See more..." : "See less"}
+        </button>
       </div>
       <div className={classNames.cardFooter}>
         {project.screenshots.map((imageUrl, index) => (
